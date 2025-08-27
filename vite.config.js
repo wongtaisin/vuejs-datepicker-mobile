509b3726f51db8ab0719244f4acccf38a849f6aa
wingddd <wongtaisin1024@gmail.com>
2025-08-27 15:05:12 +0800

    修正npm 打包 lib文件

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,11 @@ export default defineConfig(({ mode }) => {
     return {
       plugins: [vue()],
       build: {
+        outDir: 'lib', // 输出到lib目录而不是默认的dist目录
         lib: {
           entry: path.resolve(__dirname, 'src/index.js'),
           name: 'VuejsDatepickerMobile',
-          fileName: (format) => `vuejs-datepicker-mobile.${format}.js`,
+          fileName: (format) => `vuejs-datepicker-mobile.${format}.min.js`, // 添加.min.js后缀
         },
         rollupOptions: {
           // 确保外部化处理那些你不想打包进库的依赖
@@ -22,7 +23,8 @@ export default defineConfig(({ mode }) => {
               vue: 'Vue'
             }
           }
-        }
+        },
+        minify: 'terser' // 确保代码被压缩
       }
     }
   }
